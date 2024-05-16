72039b27eb79f5e98599c876dfeb8c2eb74737b1
V0vaG <its_a_vio@hotmail.com>
2024-05-16 22:02:31 +0300

    auto_push

--- vova_sphere_v4/auto_git.sh
+++ vova_sphere_v4/auto_git.sh
@@ -2,13 +2,19 @@
  
 version='1.0.0'
  
-if [[ $1 == '-v' ]]; then
-	echo $version
-	exit
-elif [[ $1 == '-h' ]]; then
-	echo "help"
-	exit
-fi
+help(){
+echo '> command: a_git
+flags:
+$ a_git [-flag]
+> [-push] git add, commit & push to all repos from "git_list" 
+> [-pull] git fetch & pull from all repos from "git_list" 
+
+$ a_git [-flag] [option]
+> [-c] add cronjob, then enter [option]
+    options:
+    [-push] add push cronjob
+    [-pull] add pull cronjob'
+}
  
 git_list=(
 	'/home/vova/new/GIT/do'
@@ -16,24 +22,30 @@ git_list=(
 	'/home/vova/new/GIT/vladimi.glayzer'
 	'/home/vova/new/GIT/vova_sphere'
 )
-
-
-if [ $1 == "push" ]; then
+if [ ! $1 ]; then
+	echo "Enter a flag or -h for help"
+elif [ $1 == "-h" ]; then
+	help
+	exit
+elif [ $1 == "-v" ]; then
+	echo $version
+	exit
+elif [ $1 == "-push" ]; then
 	for git_ripo in "${git_list[@]}"; do
 		echo "************************************"
 		echo "pushing to $git_ripo"
 		cd $git_ripo && git add . && git commit -m 'auto_push' && git push
 	done
-elif [ $1 == "pull" ]; then
+elif [ $1 == "-pull" ]; then
 	for git_ripo in "${git_list[@]}"; do
 		echo "************************************"
 		echo "puling from $git_ripo"
 		cd $git_ripo && git fetch && git pull
 	done
 elif [ $1 == "-c" ]; then
-	if [ $2 == "push" ];then
+	if [ $2 == "-push" ];then
 		(crontab -l ; echo '10 23 * * * /bin/bash /home/vova/my_scripts/auto_git/auto_git.sh push') | crontab
-	elif [ $2 == "pull" ]; then
+	elif [ $2 == "-pull" ]; then
 		(crontab -l ; echo '10 23 * * * /bin/bash /home/vova/my_scripts/auto_git/auto_git.sh pull') | crontab
 	fi
 fi
